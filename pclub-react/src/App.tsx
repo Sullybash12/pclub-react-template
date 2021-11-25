59497e0f16d7e75f1f872110d7c56079000562aa
Sebastian <Sullybash1234@gmail.com>
2021-11-24 17:43:43 -0700

    Epic particles

--- pclub-react/src/App.tsx
+++ pclub-react/src/App.tsx
@@ -3,24 +3,116 @@ import Particles from 'react-tsparticles';
 
 export default function App() {
     return (
-        <div className="App">
-            <h1>Hello CodeSandbox</h1>
-            <h2>Start editing to see some magic happen!</h2>
+        <div>
             <Particles
                 params={{
-                    fpsLimit: 60,
+                    fpsLimit: 144,
                     particles: {
-                        color: {
-                            value: '#000',
+                        number: {
+                            value: 80,
+                            density: {
+                                enable: true,
+                                value_area: 800,
+                            },
                         },
-                        links: {
+                        color: {
+                            value: '#5ab9ea',
+                        },
+                        shape: {
+                            type: 'circle',
+                            stroke: {
+                                width: 0,
+                                color: '#000000',
+                            },
+                            polygon: {
+                                nb_sides: 5,
+                            },
+                            image: {
+                                src: 'img/github.svg',
+                                width: 100,
+                                height: 100,
+                            },
+                        },
+                        opacity: {
+                            value: 0.5,
+                            random: false,
+                            anim: {
+                                enable: false,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: false,
+                            },
+                        },
+                        size: {
+                            value: 3,
+                            random: true,
+                            anim: {
+                                enable: false,
+                                speed: 40,
+                                size_min: 0.1,
+                                sync: false,
+                            },
+                        },
+                        line_linked: {
                             enable: true,
-                            color: '#000',
                             distance: 150,
+                            color: '#66fcf1',
+                            opacity: 0.8128220708812408,
+                            width: 1,
                         },
                         move: {
                             enable: true,
+                            speed: 6,
+                            random: false,
+                            straight: false,
+                            bounce: false,
+                            attract: {
+                                enable: false,
+                                rotateX: 600,
+                                rotateY: 1200,
+                            },
                         },
+                    },
+                    interactivity: {
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: 'repulse',
+                            },
+                            onclick: {
+                                enable: true,
+                                mode: 'push',
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            grab: {
+                                distance: 400,
+                                line_linked: {
+                                    opacity: 1,
+                                },
+                            },
+                            bubble: {
+                                distance: 400,
+                                size: 40,
+                                duration: 2,
+                                opacity: 8,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                            push: {
+                                particles_nb: 4,
+                            },
+                            remove: {
+                                particles_nb: 2,
+                            },
+                        },
+                    },
+                    retina_detect: true,
+                    background: {
+                        color: '#0b0c10',
                     },
                 }}
             />
