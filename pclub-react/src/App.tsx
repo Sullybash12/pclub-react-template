e2c16b84557d2290191e33ae33e61c56fc1a73ca
Sebastian <Sullybash1234@gmail.com>
2021-11-29 18:20:56 -0700

    Organization

--- pclub-react/src/App.tsx
+++ pclub-react/src/App.tsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
+import React from 'react';
 import Particles from 'react-tsparticles';
 import particlesConfig from './assets/particlesConfig.json';
+import './assets/stylesheets/App.css';
 
 export default function App() {
     return (
         <div className="container">
-            <Particles params={particlesConfig} />
-            <h1 style={{ color: '#c5c6c7', position: 'absolute' }}>
-                BOV Programming Club
-            </h1>
+            <Particles params={particlesConfig} className="app-particles" />
+            <div className="textOverlay">
+                <h1 style={{ color: '#c5c6c7' }}>BOV Programming Club</h1>
+            </div>
         </div>
     );
 }
